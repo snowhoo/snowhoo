b1b7853f384dee4c31077c0cfbf3a375a31859a4
snowhoo <snowhoo@example.com>
2026-05-09 17:02:27 +0800

    cleanup: remove debug/test files from email-bot

--- hexo-bot/email-bot/run-test.js
+++
@@ -1,7 +0,0 @@
-process.chdir('D:/hexo/hexo-bot/email-bot');
-try {
-  require('./test-convert.js');
-} catch(e) {
-  process.stderr.write('FATAL: ' + e.message + '\n' + e.stack + '\n');
-  process.exit(1);
-}
