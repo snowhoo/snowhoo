0184437cb4dd66ff389fdd561efa1076d626c28d
snowhoo <snowhoo@example.com>
2026-05-14 10:44:03 +0800

    fix: add chcp 65001 to bat files, prevent encoding crash

--- hexo-bot/tasks/run-peopleapp-yedu.bat
+++ hexo-bot/tasks/run-peopleapp-yedu.bat
@@ -1,8 +1,11 @@
 @echo off
-title PeopleApp 人民夜读采集
-set "BOT_DIR=%~dp0..\yedu-peopleapp"
-set "LOG=%BOT_DIR%\cron.log"
-echo [%DATE% %TIME%] ======== 开始 ======== >> "%LOG%"
+chcp 65001 >nul
+title PeopleAppYeDu
+
+set BOT_DIR=%~dp0..\yedu-peopleapp
+set LOG=%BOT_DIR%\cron.log
+
+echo [%DATE% %TIME%] ======== START ======== >> "%LOG%"
 "C:\Program Files\nodejs\node.exe" "%BOT_DIR%\fetch-peopleapp-yedu.js" >> "%LOG%" 2>&1
-echo [%DATE% %TIME%] ======== 结束 ======== >> "%LOG%"
+echo [%DATE% %TIME%] ======== DONE ======== >> "%LOG%"
 echo. >> "%LOG%"
