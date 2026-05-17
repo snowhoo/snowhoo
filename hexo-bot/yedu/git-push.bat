ec917c59ce3eef6564b869942b1492317a0ee0ba
snowhoo <snowhoo@example.com>
2026-05-18 01:10:40 +0800

    fix compress layout

--- hexo-bot/yedu/git-push.bat
+++ hexo-bot/yedu/git-push.bat
@@ -2,4 +2,4 @@
 chcp 65001 >nul
 git -C D:\hexo\hexo-bot\yedu add -A
 git -C D:\hexo\hexo-bot\yedu commit -m "fix compress layout"
-git -C D:\hexo\hexo-bot\yedu push origin master
+git -C D:\hexo\hexo-bot\yedu push origin main
